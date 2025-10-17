cb1a00e243b787c7fa497eac2077b0c84a27e658
Austin Ewell <austin.ewell86@gmail.com>
2025-10-17 15:30:32 -0600

    removed old files refactored layout

---
+++ frontend/components/Layout/LayoutHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="bg-gray-800 text-white p-4 flex items-center gap-2">
+        <img
+            class="w-14 h-14"
+            :src="smallLogo"
+            alt="Small Logo"
+        />
+        <img
+            class="h-8 sm:hidden lg:block"
+            :src="textLogo"
+            alt="Text Logo"
+        />
+    </header>
+</template>
+
+<script setup>
+const smallLogo = '/images/small-logo.png'
+const textLogo = '/images/text-logo.png'
+</script>
